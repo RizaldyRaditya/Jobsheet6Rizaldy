b9c5a6a072d44ac5e80023935cb56862cdf0164c
Rudi Erwanto <Rudi Erwanto@DESKTOP-86HM3LO>
2018-08-02 12:27:55 +0700

    Initial Commmit

---
+++ src/latihan/ContohFormatString.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author Rudi Erwanto
+ */
+public class ContohFormatString {
+    public static void main(String[] args) {
+        String namaDepan = "Rizaldy";
+        String namaBelakang = "Raditya";
+        String namaAkhir = "Erwanto";
+        System.out.format("Nama saya %s %s %s %n", namaDepan, namaBelakang, namaAkhir);
+    }
+
+}
---
+++ src/latihan/Kondisi.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rudi Erwanto
+ */
+public class Kondisi {
+    public static void main(String[] args) {
+        Scanner masukan = new Scanner(System.in);
+        int nilai;
+        System.out.print("Masukan nilai akhir PBO: ");
+        nilai = masukan.nextInt();
+        if (nilai < 70)
+            System.out.println("SISWA TIDAK LULUS");
+        if (nilai > 70)
+            System.out.println("SISWA LULUS");
+    }
+}
